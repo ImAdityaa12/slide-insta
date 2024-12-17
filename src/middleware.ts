dacb2583754e1e89ae2e9bdaed47e8f26aeb828b
ImAdityaa12 <adityagupta1291@gmail.com>
2024-12-17 11:09:55 +0530

    Fixed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,14 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
 const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
-  "/api/payments/(.*)",
+  "/api/payment(.*)",
   "/callback(.*)",
 ]);
-export default clerkMiddleware();
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect();
+});
 
 export const config = {
   matcher: [
